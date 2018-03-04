34f78167156f44c14428609bc3f471cdb3712cd5
DmitryKobzev <dmitriykobzev99@gmail.com>
2018-03-04 20:24:14 +0300

    add main.js

---
+++ task4/main.js
@@ -0,0 +1,393 @@
+var photoPosts = [
+    {
+        id: '1',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 1, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '2',
+        description: 'World',
+        createdAt: new Date(2018, 5, 2, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '3',
+        description: 'Nature',
+        createdAt: new Date(2018, 5, 3, 11, 11, 11),
+        author: 'shnurov',
+        photoLink: 'comedy',
+        hashTags: ['#goodday'],
+        likes: ['urgant', 'lady']
+    },
+    {
+        id: '4',
+        description: 'Beauty',
+        createdAt: new Date(2018, 5, 4, 16, 12, 55),
+        author: 'lady',
+        photoLink: 'uzn',
+        hashTags: ['#luck', '#happiness'],
+        likes: ['arina', 'shnurov', 'lady']
+    },
+    {
+        id: '5',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 6, 17, 22, 19),
+        author: 'arina',
+        photoLink: 'comedy',
+        hashTags: ['#nice'],
+        likes: ['lady']
+    },
+    {
+        id: '6',
+        description: 'World',
+        createdAt: new Date(2018, 5, 7, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '7',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 8, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '8',
+        description: 'World',
+        createdAt: new Date(2018, 5, 9, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '9',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 10, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '10',
+        description: 'World',
+        createdAt: new Date(2018, 5, 11, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '11',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 12, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '12',
+        description: 'World',
+        createdAt: new Date(2018, 5, 13, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '13',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 14, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '14',
+        description: 'World',
+        createdAt: new Date(2018, 5, 15, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '15',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 16, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#nice', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '16',
+        description: 'World',
+        createdAt: new Date(2018, 5, 17, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '17',
+        description: 'Hello friends',
+        createdAt: new Date(2018, 5, 18, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '18',
+        description: 'World',
+        createdAt: new Date(2018, 5, 19, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    },
+    {
+        id: '19',
+        description: 'Hello friends',
+        createdAt: new Date(2017, 5, 20, 17, 22, 19),
+        author: '_dimakobzev',
+        photoLink: 'comedy',
+        hashTags: ['#goodday', '#smile'],
+        likes: ['urgant', 'shnurov', 'lady']
+    },
+    {
+        id: '20',
+        description: 'World',
+        createdAt: new Date(2018, 5, 21, 21, 32, 59),
+        author: 'urgant',
+        photoLink: 'uzn',
+        hashTags: ['#goodday', '#luck', '#happiness'],
+        likes: ['_dimakobzev', 'shnurov', 'lady']
+    }
+];
+var filterNickname = {
+    nickname: '_dimakobzev'
+};
+var filterHashTags = {
+    hashTags: ['#goodday', '#nice']
+};
+var filterDate = {
+    fromDate: new Date(2018, 5, 1),
+    toDate: new Date(2018, 5, 25)
+};
+var addPhoto = {
+    id: "21",
+    description: 'eqweqwe',
+    createdAt: new Date(2018, 5, 22, 16, 12, 55),
+    author: 'Dima',
+    photoLink: 'link',
+    hashTags: ['tag1', 'tag2'],
+    likes: ['me'],
+};
+var addPhotoWithInvalidArguments = {
+    description: 'eqweqwe',
+    createdAt: new Date(2018, 2, 14, 16, 12, 55),
+    author: 'Dima',
+    photoLink: 'link',
+    hashTags: ['tag1', 'tag2'],
+    likes: ['me'],
+};
+let modul = (function () {
+    var sortedPhotoPosts = photoPosts.sort((post1, post2) => {
+        return post2.createdAt - post1.createdAt;
+    });
+
+    let getPhotoPost = function (id) {
+        if (typeof id !== "string") {
+            console.log("Oops!You entered an invalid argument!");
+            return;
+        }
+        for (var i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id === id) {
+                return photoPosts[i];
+            }
+        }
+        return false;
+    }
+
+
+    let getPhotoPosts = function (skip, top, filterNickname, filterHashTags, filterDate) {
+        skip = skip || 0;
+        top = top || 10;
+        /*   filterNickname = filterNickname || {};
+           filterHashTags = filterHashTags || {};
+           filterDate = filterDate || {};*/
+        if (typeof skip !== 'number' || typeof top !== 'number') {
+            console.log("Oops!You entered an invalid argument!");
+            return;
+        }
+
+        sortedPhotoPosts = sortedPhotoPosts.filter(post => {
+            if (filterNickname !== undefined && filterNickname.hasOwnProperty("nickname")) {
+                if (filterNickname.nickname !== post.author) {
+                    return false;
+                }
+            }
+            if (filterHashTags !== undefined && filterHashTags.hasOwnProperty("hashTags")) {
+                if (!filterHashTags.hashTags.every((tag) => {
+                    return post.hashTags.includes(tag);
+                }))
+                    return false;
+            }
+            if (filterDate !== undefined && filterDate.hasOwnProperty("fromDate")) {
+                if (post.createdAt - filterDate.fromDate < 0) {
+                    return false;
+                }
+            }
+            if (filterDate !== undefined && filterDate.hasOwnProperty("toDate")) {
+                if (filterDate.toDate - post.createdAt < 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+        return sortedPhotoPosts.slice(skip, top + skip);
+    }
+
+    let validatePhotoPost = function (photoPost) {
+        if (typeof photoPost.id !== 'string' || photoPost.id === "") {
+            return false;
+        }
+        if (typeof photoPost.author !== 'string' || photoPost.author === "") {
+            return false;
+        }
+        if (typeof photoPost.description !== 'string' || photoPost.description.length > 200) {
+            return false;
+        }
+        if (!(photoPost.createdAt instanceof Date)) {
+            return false;
+        }
+        if (typeof photoPost.photoLink !== 'string' || photoPost.photoLink === '') {
+            return false;
+        }
+        if (typeof photoPost.hashTags === null) {
+            return false;
+        }
+        if (typeof photoPost.likes === null) {
+            return false;
+        }
+        return true;
+    }
+
+    let addPhotoPost = function (photoPost) {
+        if (validatePhotoPost(photoPost)) {
+            photoPosts.push(photoPost);
+            sortedPhotoPosts = photoPosts.sort((post1, post2) => {
+                return post2.createdAt - post1.createdAt;
+            });
+            return true;
+        } else return false;
+
+    }
+
+    let editPhotoPost = function (id, photoPost) {
+        var photoPostToChange = getPhotoPost(id);
+        if (!validatePhotoPost(photoPostToChange)) {
+            return false;
+        }
+        if (typeof photoPost !== "object" || photoPost.hasOwnProperty("id") || photoPost.hasOwnProperty("author") || photoPost.hasOwnProperty("createdAt")) {
+            console.log("Invalid argument! Fields {author}, {date}, {id} can not be edited.");
+            return false;
+        }
+        Object.assign(photoPostToChange, photoPost);
+        return true;
+    }
+
+
+    let removePhotoPost = function (id) {
+        if (typeof id !== "string" || id === "") {
+            console.log("Oops!You entered an invalid argument!");
+            return;
+        }
+        for (var i = 0; i < photoPosts.length; i++) {
+            if (photoPosts[i].id === id) {
+                photoPosts.splice(i, 1);
+                return "deletion was successful!";
+            }
+        }
+        return false;
+    }
+    return {
+        getPhotoPost,
+        getPhotoPosts,
+        addPhotoPost,
+        editPhotoPost,
+        removePhotoPost,
+        validatePhotoPost
+    }
+})();
+
+console.log("--addPhotoPost:-- ")
+console.log("All post: ");
+console.log(photoPosts);
+console.log("Add post: ")
+console.log(modul.addPhotoPost(addPhoto));
+console.log("All post: ");
+console.log(photoPosts);
+console.log("Add post with invalid arguments: ")
+console.log(modul.addPhotoPost(addPhotoWithInvalidArguments));
+console.log("");
+console.log("--getPhotoPosts:-- ")
+console.log("10 posts:");
+console.log(modul.getPhotoPosts());
+console.log("skip = 1 and print = 3:");
+console.log(modul.getPhotoPosts(1, 3));
+console.log("skip = 6 and top = 10 (default):");
+console.log(modul.getPhotoPosts(6));
+console.log("posts wwith filterConfig:");
+console.log(modul.getPhotoPosts(0, 10, filterNickname, {}, filterDate));
+console.log(modul.getPhotoPosts(0, 10, filterNickname, filterHashTags));
+console.log("with invalid argument:")
+console.log(modul.getPhotoPosts("Hello"))
+console.log("");
+console.log("--getPhotoPost:-- ")
+console.log("Post with id 9: ")
+console.log(modul.getPhotoPost('9'));
+console.log("Post with invalid argument: ")
+console.log(modul.getPhotoPost('59'));
+console.log("");
+console.log("--validatePhotoPost:-- ")
+console.log(modul.validatePhotoPost(photoPosts[9]));
+console.log("Post with invalid createdAt: ")
+console.log(modul.validatePhotoPost(addPhotoWithInvalidArguments));
+console.log("");
+console.log("--removePhotoPost:-- ")
+console.log("removePhotoPost with id 18: ")
+console.log(modul.removePhotoPost('18'));
+console.log("All post: ");
+console.log(photoPosts);
+console.log("removePhotoPost with invalid argument: ")
+console.log(modul.removePhotoPost('22'));
+console.log("");
+console.log("--editPhotoPost:-- ")
+console.log("post with id 2 before editing:");
+console.log(modul.getPhotoPost('2'));
+console.log("trying to edit post with id 2:");
+console.log(modul.editPhotoPost('2', { description: 'qewqwe', photoLink: 'qwewq' }));
+console.log("post with id 2 after editing:");
+console.log(modul.getPhotoPost('2'));
+console.log("trying to edit post with invalid argument:");
+console.log(modul.editPhotoPost('2', { createdAt: new Date(2018, 2, 14, 16, 12, 55), photoLink: 'qwewq', description: 'qewqwe' }));
+console.log("All post: ");
+console.log(photoPosts);
